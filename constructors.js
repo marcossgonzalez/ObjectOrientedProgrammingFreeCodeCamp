4225217cc38c02ead1b989f238a93132561e76a8
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-12 22:29:22 -0700

    Update lesson
    update lesson

---
+++ constructors.js
@@ -0,0 +1,18 @@
+//--------------------------------------
+/* Construtores são funções que criam novos objetos. Eles definem propriedades e comportamentos que 
+pertencerão ao novo objeto. Pense neles como um modelo para a criação de novos objetos.
+
+Por exemplo:
+function Bird() {
+  this.name = "Albert";
+  this.color = "blue";
+  this.numLegs = 2;
+}
+Esse construtor define 'Bird' como objeto e as palavras-chave 'this' como chaves para os valores em sequência;*/
+//--------------------------------------
+function Dog(){
+    this.name =  "Doggo";
+    this.color = "Brown";
+    this.numLegs = 4;
+}
+//--------------------------------------
--- createamethodonobject.js
+++ createamethodonobject.js
@@ -19,3 +19,23 @@ let dog = {
   
   dog.sayLegs();
 //--------------------------------------
+/*Se o nome da variável mudar, qualquer código que faça referência ao nome original também precisará ser atualizado. 
+Em uma definição curta de objeto, isso não é um problema, mas se um objeto tiver muitas referências às suas 
+propriedades, há uma chance maior de erro.
+
+Uma maneira de evitar esses problemas é com a palavra-chave 'this':
+
+let duck = {
+  name: "Aflac",
+  numLegs: 2,
+  sayName: function() {return "The name of this duck is " + (palavra-chave 'this')->>this.name + ".";}
+};*/
+//--------------------------------------
+let dog2 = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+  };
+  
+  dog2.sayLegs();
+//--------------------------------------
