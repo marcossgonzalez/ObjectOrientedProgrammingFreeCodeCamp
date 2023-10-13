08b1115cf01cd459ba6ccf27054655a371d44809
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-12 22:35:30 -0700

    Update lesson
    Update lesson

--- constructors.js
+++ constructors.js
@@ -16,3 +16,30 @@ function Dog(){
     this.numLegs = 4;
 }
 //--------------------------------------
+/* Para instanciar (ou fazer o Objeto ser alocado na memória) usamos a palavra-chave 'new'. Assim usamos o método construtor
+e o convertemos em um Objeto usável, com propriedades e valores.
+Por exemplo:
+function Bird() {
+  this.name = "Albert";
+  this.color  = "blue";
+  this.numLegs = 2;
+}
+
+let bluebird = (palavra-chave)->> new Bird();*/
+//--------------------------------------
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+  }
+  let hound = new Dog;
+  // Only change code below this line
+//--------------------------------------
+function Dog(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
+  }
+  
+  let terrier = new Dog("Terrier", "Marrom");
+//--------------------------------------
