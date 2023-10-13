74c9f2ddb8043a6764b6c89e5e0ff974b9b31f24
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-12 22:17:07 -0700

    Update lesson
    Update lesson

---
+++ createbasicobject.js
@@ -0,0 +1,28 @@
+//--------------------------------------
+/*Objetos em JavaScript são usados para modelar objetos do mundo real, dando-lhes propriedades e 
+comportamento exatamente como seus equivalentes do mundo real. 
+Aqui está um exemplo usando esses conceitos para criar um objeto pato:
+let duck = {
+  name: "Aflac",
+  numLegs: 2
+};
+
+*/
+//--------------------------------------
+let dog = {
+    name: "Doggo",
+    numLegs: 4
+  };
+//--------------------------------------
+/* A anotação com ponto é usada para acessar o valor do objeto no nome do objeto, em conjunto com o objeto.
+Por exemplo:
+console.log(duck.name);*/
+//--------------------------------------
+let dog2 = {
+    name: "Spot",
+    numLegs: 4
+  };
+  console.log(dog2.name);
+  console.log(dog2.numLegs);
+// Only change code below this line
+//--------------------------------------
