a649732b30c40246871be6beadfb89be0dc1f94d
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-14 02:26:01 -0700

    Minor fixes
    minor fixes in indentation

--- iterateoverproperties.js
+++ iterateoverproperties.js
@@ -1,38 +1,38 @@
 //--------------------------------------
 function Dog(name) {
-    this.name = name;
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+for (let props in beagle) {
+  if (beagle.hasOwnProperty(props)) {
+    ownProps.push(props);
+  } else {
+    prototypeProps.push(props);
   }
-  
-  Dog.prototype.numLegs = 4;
-  
-  let beagle = new Dog("Snoopy");
-  
-  let ownProps = [];
-  let prototypeProps = [];
-  
-  for (let props in beagle){
-    if (beagle.hasOwnProperty(props)){
-      ownProps.push(props);
-    }else{
-      prototypeProps.push(props);
-    }
-  }
-  console.log(ownProps);
-  console.log(prototypeProps);
-  
-  // Only change code below this line
+}
+console.log(ownProps);
+console.log(prototypeProps);
+
+// Only change code below this line
 //--------------------------------------
 function Dog2(name) {
-    this.name = name;
+  this.name = name;
+}
+
+// Only change code below this line
+function joinDogFraternity(candidate) {
+  if (candidate.constructor === Dog2) {
+    return true;
   }
-  
-  // Only change code below this line
-  function joinDogFraternity(candidate) {
-    if (candidate.constructor === Dog2){
-      return true;
-    }
-    return false;
-  }
+  return false;
+}
 //--------------------------------------
 /*Uma maneira mais eficiente é definir o protótipo para um novo objeto que já contenha as propriedades. 
 Dessa forma, as propriedades são adicionadas todas de uma vez:
@@ -48,21 +48,21 @@ Bird.prototype = {
 };*/
 //--------------------------------------
 function Dog3(name) {
-    this.name = name;
-  }
-  
-  Dog3.prototype = {
-    // Only change code below this line
-    numLegs: 4,
-    eat: function(){
-      console.log("slurp");
-    },
-    describe: function(){
-      console.log("My name is " + this.name);
-    }
-  };
-  let terrier = new Dog3("Snoopy");
-  terrier.describe();
+  this.name = name;
+}
+
+Dog3.prototype = {
+  // Only change code below this line
+  numLegs: 4,
+  eat: function () {
+    console.log("slurp");
+  },
+  describe: function () {
+    console.log("My name is " + this.name);
+  },
+};
+let terrier = new Dog3("Snoopy");
+terrier.describe();
 //--------------------------------------
 /*Sempre que um protótipo for definido manualmente para um novo objeto, 
 lembre-se de definir a propriedade do construtor:
@@ -78,20 +78,20 @@ Bird.prototype = {
 };*/
 //--------------------------------------
 function Dog4(name) {
-    this.name = name;
-  }
-  
-  // Only change code below this line
-  Dog4.prototype = {
-    constructor: Dog4,
-    numLegs: 4,
-    eat: function() {
-      console.log("nom nom nom");
-    },
-    describe: function() {
-      console.log("My name is " + this.name);
-    }
-  };
+  this.name = name;
+}
+
+// Only change code below this line
+Dog4.prototype = {
+  constructor: Dog4,
+  numLegs: 4,
+  eat: function () {
+    console.log("nom nom nom");
+  },
+  describe: function () {
+    console.log("My name is " + this.name);
+  },
+};
 //--------------------------------------
 /*Você pode mostrar a herança das propriedades de construtor com o método isPrototypeOf('varaqui')
 Por Exemplo:
@@ -100,11 +100,11 @@ Isso retornaria o valor booleano 'true';
 */
 //--------------------------------------
 function Dog5(name) {
-    this.name = name;
-  }
-  
-  let beagle2 = new Dog5("Snoopy");
-  
-  // Only change code below this line
-  Dog5.prototype.isPrototypeOf(beagle2);
+  this.name = name;
+}
+
+let beagle2 = new Dog5("Snoopy");
+
+// Only change code below this line
+Dog5.prototype.isPrototypeOf(beagle2);
 //--------------------------------------
