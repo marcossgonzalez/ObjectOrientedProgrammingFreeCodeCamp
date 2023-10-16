98c41579a0842a07fd968c58c2982f0c25ca8bdf
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-15 21:34:03 -0300

    Update lesson
    update lesson

--- inheritance.js
+++ inheritance.js
@@ -85,3 +85,38 @@ Dog2.prototype.constructor = Dog2;
 let beagle3 = new Dog2();
 beagle3.constructor;
 //------------------------------------------
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+Dog.prototype.bark = function(){
+  console.log("Woof!");
+}
+
+
+// Only change code above this line
+
+let beagle4 = new Dog();
+//------------------------------------------
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+Penguin.prototype.fly = function() { return "Alas, this is a flightless bird."; };
+
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+console.log(Bird.prototype.fly());
+//------------------------------------------
