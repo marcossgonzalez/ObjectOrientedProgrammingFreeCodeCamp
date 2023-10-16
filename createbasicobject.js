7e837451564feda9ba2efff157be08e517c20f09
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-15 21:44:18 -0300

    Update lesson
    update lesson

--- createbasicobject.js
+++ createbasicobject.js
@@ -26,3 +26,25 @@ let dog2 = {
   console.log(dog2.numLegs);
 // Only change code below this line
 //--------------------------------------
+let bird = {
+  name: "Donald",
+  numLegs: 2
+};
+
+let boat = {
+  name: "Warrior",
+  type: "race-boat"
+};
+
+// Only change code below this line
+let glideMixin = function(obj){
+  obj.glide = function(){
+    console.log("I'm gliding!");
+  }
+}
+
+glideMixin(boat);
+glideMixin(bird);
+
+console.log(bird.glide());
+//--------------------------------------
